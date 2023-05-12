31371ed0ad840717baf51cbd0d66ba8d28102ea1
Idrissa <idrisabkar2018@gmail.com>
2023-05-12 17:27:22 +0300

    ajac

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-var word = ""
+var word = "";
+
 $(document).keypress(function (e) { 
-    word = $("#word").val()    
-    if (e.which === 13 && word != "") {
+    word = $("#word").val();    
+    if (e.which === 13 && word !== "") {
+        $(".response").text("searching...");
         $.ajax({
             type: "get",
             url: "https://api.dictionaryapi.dev/api/v2/entries/en/" + word,
             success: function (response) {
-                console.log(response[0].meanings[0].definitions[0].definition);
+                var meaning = response[0].meanings[0].definitions[0].definition;
+                var url = response[0].phonetics[0].audio;
+                $(".response").html(
+                    '<div class="response">' +
+                        '<h6>Word title : <strong>' + word + '</strong></h6>' +
+                        '<h6>Meaning: <strong>' + meaning + '</strong></h6>' +
+                        '<div class="container">' +
+                            '<audio src="' + url + '" controls class="my-4"></audio>' +
+                        '</div>' +
+                    '</div>'
+                );
             },
             error: function (xhr, status, error) {
                 console.log("Error:", error);
@@ -14,29 +26,3 @@ $(document).keypress(function (e) {
         });
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//<div class="response">
-  //          <h6>Word title : <strong>Word</strong></h6>
-  //          <h6>Meaning: <strong>Word</strong></h6>
-  //          <div class="container">
-  //              <audio src="audio-file.mp3" controls class="my-4"></audio>
-  //            </div>
-//</div>
